e9ecc62f87abd4122d75af2d6deaeb5650c3e6b0
Sebastián Albacura <sebastianalbacura24@gmail.com>
2025-12-01 21:13:40 -0500

    nuevo3

--- Api/Program.cs
+++ Api/Program.cs
@@ -17,7 +17,7 @@ namespace Api
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();  
+            builder.Services.AddSwaggerGen();   
             builder.Services.AddControllers().AddNewtonsoftJson(
                  options =>
                  options.SerializerSettings.ReferenceLoopHandling
